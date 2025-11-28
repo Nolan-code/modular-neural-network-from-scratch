bf08488b1734d707243c7fbddd9421171cc77e6d
Nolan_TPS <nolanletyrant@gmail.com>
2025-11-29 00:32:29 +0100

    Add files via upload

---
+++ src/activations.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+class ReLU:
+    def Forward_prop(self, Z): 
+      self.Z = Z
+      self.A = np.maximum(0,Z)
+      return np.maximum(0, Z)
+
+    def Backward_prop(self, dA): 
+      return dA*(self.A > 0)
+
+class Sigmoid:
+    def Forward_prop(self, Z):
+        self.A = 1 / (1 + np.exp(-Z))
+        return self.A
+
+    def Backward_prop(self, dA):
+        return dA * (self.A * (1 - self.A))
+
+
+class Softmax:
+    def Forward_prop(self, Z): 
+        exp = np.exp(Z - np.max(Z, axis=1, keepdims=True))
+        return exp / np.sum(exp, axis=1, keepdims=True)
